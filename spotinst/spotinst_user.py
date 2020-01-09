2775eb83d25a739c1e928e693462186b99e45899
Marco Tulio R Braga <marco.tulio@mtulio.eng.br>
2020-01-09 16:42:13 -0300

    fix/modue-user: add generating user token

--- spotinst/spotinst_user.py
+++ spotinst/spotinst_user.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
-# Copyright (c) 2017 Ansible Project
+# Copyright (c) 2020 Ansible Project
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 from __future__ import (absolute_import, division, print_function)
+
 
 ANSIBLE_METADATA = {'metadata_version': '1.1',
                     'status': ['preview'],
                     'supported_by': 'community'}
+
+
 DOCUMENTATION = """
 ---
 module: spotinst_user
@@ -77,10 +81,9 @@ options:
     description:
       - List of role Mapping
 """
-EXAMPLES = """
 
+EXAMPLES = '''
 # create user and assigned to Account with Role mapping
-
 - spotinst_user:
     email: "{{ item.email }}"
     first_name: "{{ item.first_name }}"
@@ -99,7 +102,16 @@ EXAMPLES = """
           "account_name": "x-test-01",
           "role": "viewer"
       }]
-"""
+
+# remove user
+- spotinst_user:
+    email: "{{ item.email }}"
+    state: absent
+  with_items:
+    - email: firstName.lastName@mydomain.com.br
+    - email: secondUser@mydomain.com.br
+
+'''
 RETURN = """
 ---
 result:
@@ -117,63 +129,18 @@ import time
 import json
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils.basic import env_fallback
+from ansible.module_utils.urls import open_url
 
 try:
     import spotinst_sdk as spotinst
     from spotinst_sdk import SpotinstClientException
 
     HAS_SPOTINST_SDK = True
-
 except ImportError:
     pass
 
 
-def handle_user(client, module):
-    message = None
-    has_changed = False
-    request_type = module.params.get('state')
-
-    if request_type == "present":
-        account, message, has_changed = handle_create(client=client, module=module)
-    elif request_type == "absent":
-        account, message, has_changed = handle_delete(client=client, module=module)
-    else:
-        module.fail_json(msg="Request {} Not Allowed for Account {}".format(request_type, account))
-
-    return account, message, has_changed
-
-
-def handle_create(client, module):
-
-    has_changed = False
-    try:
-      found, user = find_user(client, module)
-      if not found:
-        resp, message, has_changed = create_user(client, module)
-        if resp["code"] == 200:
-          message = 'User Created successfully'
-
-        resp, message, has_changed = mapping_user(client, module, message, has_changed)
-        if resp["code"] == 200:
-          message = 'User Created successfully'
-
-      else:
-        resp, message, has_changed = update_user_mapping(client, module)
-        if resp["code"] == 200:
-          message = 'User Updated successfully'
-
-      found, user = find_user(client, module)
-      if found:
-        user = {
-          "email": module.params.get('email'),
-          "roles": user
-        }
-
-    except Exception as e:
-      module.fail_json(msg="Error getting Spotinst User mapping: {}".format(e))
-
-    return user, message, has_changed
-
+## START SHARED IN MODULES
 
 def get_ExceptionMessage(e):
     errmsg, payload = e.message.split('\n')
@@ -193,6 +160,215 @@ def get_ExceptionErrorCode(err):
   return errCode
 
 
+def retErrMessage(message, debugErr=None):
+  """Create the error message """
+  e = {
+    "err": True,
+    "errMsg": message
+  }
+  if debugErr:
+    e["errDebug"] = debugErr
+  return e
+
+
+def getConfigCredentials(credentials_path=None):
+    # Retrieve creds file variables
+    creds_file_loaded_vars = dict()
+    if credentials_path is not None:
+        try:
+            with open(credentials_path, "r") as creds:
+                for line in creds:
+                    eq_index = line.find('=')
+                    var_name = line[:eq_index].strip()
+                    var_value = line[eq_index + 1:].strip()
+                    creds_file_loaded_vars[var_name] = var_value
+        except IOError:
+            pass
+
+    return creds_file_loaded_vars
+
+
+def getOAuthTokenFromVar():
+    """ TODO """
+    return
+
+
+def requestOAuthToken():
+    """
+    Generate a new bearer token and return it.
+    """
+    sp_username = os.getenv("SPOTINST_USER")
+    sp_password = os.getenv("SPOTINST_PASSWORD")
+    sp_client_id = os.getenv("SPOTINST_CLIENT_ID")
+    sp_client_sec = os.getenv("SPOTINST_CLIENT_SECRET")
+
+    if (not sp_username) or (not sp_password):
+      return {
+        "err": True,
+        "errMsg": "SPOTINST_USER or SPOTINST_PASSWORD env var not found"
+      }
+    if (not sp_client_id) or (not sp_client_sec):
+      return {
+        "err": True,
+        "errMsg": "SPOTINST_CLIENT_ID or SPOTINST_CLIENT_SECRET env var not found"
+      }
+
+    headers = {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+    payload = "username={}&password={}&grant_type=password&client_id={}&client_secret={}".format(
+      sp_username, sp_password, sp_client_id, sp_client_sec
+    )
+    authURL = "https://oauth.spotinst.io/token"
+
+    try:
+      resp = open_url(authURL, method="POST", data=payload, headers=headers)
+    except Exception as e:
+      return retErrMessage("Unable to open request to retrieve token", debugErr=e)
+
+    try:
+      respJS = json.loads(resp.read())
+      if "response" not in respJS:
+        return retErrMessage("Unable to get response payload", debugErr=e)
+      try:
+        accessToken = respJS["response"]["items"][0]["accessToken"]
+      except Exception as e:
+        return retErrMessage("Unable to get access token", debugErr=e)
+
+      return accessToken
+
+    except Exception as e:
+      return retErrMessage("Unable to parse token response", debugErr=e)
+
+    return None
+
+
+def getOAuthUserToken():
+    """
+    - [TODO] Get local TOKEN (already generated), if not found, then
+    - Generate a new token from user and pass
+
+    """
+
+    sp_user_token = os.getenv("SPOTINST_USER_TOKEN")
+    sp_user_token_ttl = os.getenv("SPOTINST_USER_TOKEN_TTL")
+    if not sp_user_token:
+      return requestOAuthToken()
+
+    return retErrMessage("Unavailable option to get current token", debugErr=e)
+
+
+def get_client(module):
+    """
+    Credentials order (first is higher):
+    - module argument
+    - token env vars (TODO)
+    - user and pass credentials to request a new token
+    - credentials file
+    """
+    try:
+      token = module.params.get('token')
+      if (not token):
+          token = getOAuthUserToken()
+          if not token:
+              # Retrieve creds file variables
+              creds_file_loaded_vars = getConfigCredentials(
+                credentials_path=module.params.get('credentials_path')
+              )
+              token = creds_file_loaded_vars.get("token")
+    except Exception as e:
+      module.fail_json(msg="ERROR: {}".format(e))
+
+    account = module.params.get('account_id')
+    client = spotinst.SpotinstClient(
+        auth_token=token,
+        account_id=account,
+        print_output=True,
+      )
+
+    return client
+
+## END SHARED
+
+## Handlers
+def handle_user(client, module):
+    message = None
+    has_changed = False
+    request_type = module.params.get('state')
+
+    if request_type == "present":
+        account, message, has_changed = handle_create(client=client, module=module)
+    elif request_type == "absent":
+        account, message, has_changed = handle_delete(client=client, module=module)
+    else:
+        module.fail_json(msg="Request {} Not Allowed for Account {}".format(request_type, account))
+
+    return account, message, has_changed
+
+
+def handle_create(client, module):
+    """ Create User
+
+    """
+    has_changed = False
+    try:
+      found, user = find_user(client, module)
+      if not found:
+        resp, message, has_changed = create_user(client, module)
+        # if resp["code"] == 200:
+        message = 'User Created successfully'
+
+        resp, message, has_changed = mapping_user(client, module, message, has_changed)
+        #if resp["code"] == 200:
+        message = 'User Created successfully'
+
+      else:
+        resp, message, has_changed = update_user_mapping(client, module)
+        # if resp["code"] == 200:
+        message = 'User Updated successfully'
+
+      found, user = find_user(client, module)
+      if found:
+        user = {
+          "email": module.params.get('email'),
+          "roles": user
+        }
+
+    except spotinst.SpotinstClientException as e:
+      errMsg = get_ExceptionMessage(e)
+      module.fail_json(msg="Create Handler1> {}".format(errMsg))
+
+    except Exception as e:
+      module.fail_json(msg="Create Handler2> {} {}".format(type(e), e))
+
+    return user, message, has_changed
+
+
+def handle_delete(client, module):
+    """ Delete User
+
+    """
+    user = module.params.get('email')
+    has_changed = False
+
+    try:
+      found, user = find_user(client, module)
+      if found:
+        resp, message, has_changed = delete_user(client, module)
+        if resp["code"] == 200:
+          message = 'User Removed successfully'
+      else:
+        message = 'User {} Not Found'.format(user)
+        has_changed = False
+
+
+    except Exception as e:
+      module.fail_json(msg="Error getting Spotinst User mapping: {}".format(e))
+
+    return user, message, has_changed
+
+
+## Middleaware
 def find_user(client, module):
     email = module.params.get('email')
 
@@ -228,6 +404,26 @@ def create_user(client, module):
       message = "User Created successfully"
 
     except spotinst.SpotinstClientException as e:
+      err = get_ExceptionMessage(e)
+      module.fail_json(msg="{}: {}".format(err["err"], json.dumps(err["payload"])))
+
+    except Exception as e:
+      module.fail_json(msg="Error while creating User: {}".format(e))
+
+    return resp, message, has_changed
+
+
+def delete_user(client, module):
+
+    email = module.params.get('email')
+    has_changed = True
+
+    try:
+      resp = client.detach_user(email)
+
+      message = "User Detached successfully"
+
+    except spotinst.SpotinstClientException as e:
       errMsg = get_ExceptionMessage(e)
       errors = []
       for er in errMsg["payload"]["errors"]:
@@ -247,8 +443,8 @@ def mapping_user(client, module, message, has_changed):
       accountUserMapping = []
       for m in role_mapping:
         accountUserMapping.append({
-          "user_email": email,
-          "account_id": m["account_id"],
+          "userEmail": email,
+          "accountId": m["account_id"],
           "role": m["role"]
         })
 
@@ -260,54 +456,44 @@ def mapping_user(client, module, message, has_changed):
 
 
 def update_user_mapping(client, module):
+    """
+    Steps TODO:
+    - Get User
+    - Compare user mapping in the ver
+    - Check if has changes
+    - send to mapping_user user ensure that all maps
+
+    Problem using 'update_user_role':
+    - if the user is not in the Account, it will fail (It could be an bug).
+    ERROR:
+      Error encountered while updating user:
+      [u'There is no mapping with user teste-01@domain.com and account act-XID.']
+    """
     role_mapping = module.params.get('role_mapping')
     email = module.params.get('email')
+    message = "Update User mapping is not supported"
+    resp = {}
+    has_changed = False
 
     try:
-      accountUserMapping = []
       for m in role_mapping:
-        resp = client.account_id = m["account_id"]
+        client.account_id = m["account_id"]
         resp = client.update_user_role(email, m["role"])
         has_changed = True
         message = 'User Mapping Updated Successful'
+
+    except spotinst.SpotinstClientException as e:
+      errMsg = get_ExceptionMessage(e)
+      errors = []
+      for er in errMsg["payload"]["errors"]:
+        errors.append(er["message"])
+
+      pass
+
     except Exception as e:
       module.fail_json(msg="Error Updating User Map: {}".format(e))
 
     return resp, message, has_changed
-
-
-def get_client(module):
-    # Retrieve creds file variables
-    creds_file_loaded_vars = dict()
-
-    credentials_path = module.params.get('credentials_path')
-
-    if credentials_path is not None:
-        try:
-            with open(credentials_path, "r") as creds:
-                for line in creds:
-                    eq_index = line.find('=')
-                    var_name = line[:eq_index].strip()
-                    string_value = line[eq_index + 1:].strip()
-                    creds_file_loaded_vars[var_name] = string_value
-        except IOError:
-            pass
-    # End of creds file retrieval
-
-    token = module.params.get('token')
-    if not token:
-        token = creds_file_loaded_vars.get("token")
-
-    account = module.params.get('account_id')
-    if not account:
-        account = creds_file_loaded_vars.get("account")
-
-    client = spotinst.SpotinstClient(auth_token=token, print_output=False)
-
-    if account is not None:
-        client = spotinst.SpotinstClient(auth_token=token, account_id=account, print_output=False)
-
-    return client
 
 
 def main():
@@ -322,6 +508,7 @@ def main():
         password=dict(type='str', no_log=True),
         role=dict(type='str', choices=['admin', 'viewer'], default="viewer"),
         role_mapping=dict(type='list'),
+        account_id=dict(type='str'),
     )
 
     module = AnsibleModule(argument_spec=fields)
